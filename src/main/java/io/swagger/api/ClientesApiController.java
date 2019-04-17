497f7664c27d7a5cf65bf6b789bd03d9435e58d0
André Luiz Morita <andreluizmorita@gmail.com>
2019-04-17 13:38:06 -0300

    Add DAO

--- src/main/java/io/swagger/api/ClientesApiController.java
+++ src/main/java/io/swagger/api/ClientesApiController.java
@@ -1,54 +1,72 @@
 package io.swagger.api;
 
-import io.swagger.model.Clientes;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import io.swagger.annotations.*;
+import java.util.List;
+
+import javax.servlet.http.HttpServletRequest;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestHeader;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RequestPart;
-import org.springframework.web.multipart.MultipartFile;
 
-import javax.validation.constraints.*;
-import javax.validation.Valid;
-import javax.servlet.http.HttpServletRequest;
-import java.io.IOException;
-import java.util.List;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import io.swagger.api.dao.ClienteDAO;
+import io.swagger.model.Cliente;
+import io.swagger.model.Clientes;
+
 @javax.annotation.Generated(value = "io.swagger.codegen.languages.SpringCodegen", date = "2019-04-17T15:51:38.272Z")
 
 @Controller
 public class ClientesApiController implements ClientesApi {
 
-    private static final Logger log = LoggerFactory.getLogger(ClientesApiController.class);
+	private static final Logger log = LoggerFactory.getLogger(ClientesApiController.class);
 
-    private final ObjectMapper objectMapper;
+	private final ObjectMapper objectMapper;
 
-    private final HttpServletRequest request;
+	private final HttpServletRequest request;
 
-    @org.springframework.beans.factory.annotation.Autowired
-    public ClientesApiController(ObjectMapper objectMapper, HttpServletRequest request) {
-        this.objectMapper = objectMapper;
-        this.request = request;
-    }
+	private ClienteDAO clienteDAO;
 
-    public ResponseEntity<Clientes> consultarClientes() {
-        String accept = request.getHeader("Accept");
-        if (accept != null && accept.contains("application/json")) {
-            try {
-                return new ResponseEntity<Clientes>(objectMapper.readValue("\"\"", Clientes.class), HttpStatus.NOT_IMPLEMENTED);
-            } catch (IOException e) {
-                log.error("Couldn't serialize response for content type application/json", e);
-                return new ResponseEntity<Clientes>(HttpStatus.INTERNAL_SERVER_ERROR);
-            }
-        }
+	@org.springframework.beans.factory.annotation.Autowired
+	public ClientesApiController(ObjectMapper objectMapper, HttpServletRequest request) {
+		this.objectMapper = objectMapper;
+		this.request = request;
+		this.clienteDAO = new ClienteDAO();
+	}
 
-        return new ResponseEntity<Clientes>(HttpStatus.NOT_IMPLEMENTED);
-    }
+	public ResponseEntity<Clientes> consultarClientes() {
+		ResponseEntity<Clientes> responseEntity = null;
+
+		try {
+
+			List<Cliente> clientes = clienteDAO.todos();
+			
+			// 404 NENHUM REGISTRO ENCONTRADO
+			if (clientes != null) {
+
+				if (clientes.size() <= 0) {
+
+					responseEntity = new ResponseEntity<Clientes>(
+							objectMapper.readValue(objectMapper.writeValueAsString(clientes), Clientes.class),
+							HttpStatus.NOT_FOUND);
+				}
+			// 200 SUCESSO AO CONSULTAR
+			} else {
+				
+				responseEntity = new ResponseEntity<Clientes>(
+						objectMapper.readValue(objectMapper.writeValueAsString(clientes), Clientes.class),
+						HttpStatus.OK);
+			}
+
+		} catch (Exception e) {
+			log.error("Erro ao tentar consultar clientes.");
+			responseEntity = new ResponseEntity<Clientes>(HttpStatus.INTERNAL_SERVER_ERROR);
+		}
+
+		return responseEntity;
+
+	}
 
 }
